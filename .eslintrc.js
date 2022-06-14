a768cc4e289e2babaeefc37fe8ef87554b9520e8
DaiichiroNoda <69710300+donachii@users.noreply.github.com>
2022-06-14 17:28:09 +0900

    fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,11 @@ module.exports = {
     mocha: true,
     node: true,
   },
+  globals: {
+    web3: true,
+    assert: true,
+    contract: true,
+  },
   plugins: ["@typescript-eslint"],
   extends: [
     "standard",
@@ -16,9 +21,23 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
+    "node/no-unpublished-require": ["error", { devDependencies: true }],
     "node/no-unsupported-features/es-syntax": [
       "error",
       { ignores: ["modules"] },
     ],
+    "max-len": [2, { code: 150, ignoreComments: true }],
+    quotes: [2, "double", "avoid-escape"],
+    "no-console": "off",
+    "no-underscore-dangle": "off",
+    "no-await-in-loop": "off",
+    "no-restricted-syntax": "off",
+    "guard-for-in": "off",
+    "function-paren-newline": "off",
+    "comma-dangle": "off",
+    "object-curly-newline": "off",
+    "no-unused-expressions": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/no-unresolved": "off",
   },
 };
--- utils/utilities.js
+++ utils/utilities.js
@@ -28,19 +28,41 @@ const utilities = {
 
   numberToBytes32: (input) => `0x${new BN(input).toString(16, 64)}`,
 
-  waitForUserInput: (text) => new Promise((resolve) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    rl.question(text, (answer) => {
-      resolve(answer);
-      rl.close();
-    });
-  }),
+  waitForUserInput: (text) =>
+    new Promise((resolve) => {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      rl.question(text, (answer) => {
+        resolve(answer);
+        rl.close();
+      });
+    }),
 
-  signOffchain: async (signers, from, value, data, chainId, nonce, gasPrice, gasLimit, refundToken, refundAddress) => {
-    const messageHash = utilities.getMessageHash(from, value, data, chainId, nonce, gasPrice, gasLimit, refundToken, refundAddress);
+  signOffchain: async (
+    signers,
+    from,
+    value,
+    data,
+    chainId,
+    nonce,
+    gasPrice,
+    gasLimit,
+    refundToken,
+    refundAddress
+  ) => {
+    const messageHash = utilities.getMessageHash(
+      from,
+      value,
+      data,
+      chainId,
+      nonce,
+      gasPrice,
+      gasLimit,
+      refundToken,
+      refundAddress
+    );
     const signatures = await Promise.all(
       signers.map(async (signer) => {
         const sig = await utilities.signMessage(messageHash, signer);
@@ -52,7 +74,17 @@ const utilities = {
     return joinedSignatures;
   },
 
-  getMessageHash: (from, value, data, chainId, nonce, gasPrice, gasLimit, refundToken, refundAddress) => {
+  getMessageHash: (
+    from,
+    value,
+    data,
+    chainId,
+    nonce,
+    gasPrice,
+    gasLimit,
+    refundToken,
+    refundAddress
+  ) => {
     const message = `0x${[
       "0x19",
       "0x00",
@@ -65,7 +97,9 @@ const utilities = {
       ethers.utils.hexZeroPad(ethers.utils.hexlify(gasLimit), 32),
       refundToken,
       refundAddress,
-    ].map((hex) => hex.slice(2)).join("")}`;
+    ]
+      .map((hex) => hex.slice(2))
+      .join("")}`;
 
     const messageHash = ethers.utils.keccak256(message);
     return messageHash;
@@ -79,20 +113,24 @@ const utilities = {
     return normalizedSig;
   },
 
-  personalSign: async (signHash, signer) => ethers.utils.joinSignature(signer.signingKey.signDigest(signHash)),
+  personalSign: async (signHash, signer) =>
+    ethers.utils.joinSignature(signer.signingKey.signDigest(signHash)),
 
-  sortWalletByAddress: (wallets) => wallets.sort((s1, s2) => {
-    const bn1 = ethers.BigNumber.from(s1);
-    const bn2 = ethers.BigNumber.from(s2);
-    if (bn1.lt(bn2)) return -1;
-    if (bn1.gt(bn2)) return 1;
-    return 0;
-  }),
+  sortWalletByAddress: (wallets) =>
+    wallets.sort((s1, s2) => {
+      const bn1 = ethers.BigNumber.from(s1);
+      const bn2 = ethers.BigNumber.from(s2);
+      if (bn1.lt(bn2)) return -1;
+      if (bn1.gt(bn2)) return 1;
+      return 0;
+    }),
 
   // Parses the RelayerModule.execute receipt to decompose the success value of the transaction
   // and additionally if an error was raised in the sub-call to optionally return that
   parseRelayReceipt: (txReceipt) => {
-    const { args } = txReceipt.logs.find((e) => e.event === "TransactionExecuted");
+    const { args } = txReceipt.logs.find(
+      (e) => e.event === "TransactionExecuted"
+    );
 
     let errorBytes;
     let error;
@@ -100,10 +138,14 @@ const utilities = {
       if (args.returnData.startsWith("0x08c379a0")) {
         // Remove the encoded error signatures 08c379a0
         const noErrorSelector = `0x${args.returnData.slice(10)}`;
-        const errorBytesArray = ethers.utils.defaultAbiCoder.decode(["bytes"], noErrorSelector);
+        const errorBytesArray = ethers.utils.defaultAbiCoder.decode(
+          ["bytes"],
+          noErrorSelector
+        );
         errorBytes = errorBytesArray[0]; // eslint-disable-line prefer-destructuring
       } else {
-        errorBytes = args.returnData; console.log(errorBytes);
+        errorBytes = args.returnData;
+        console.log(errorBytes);
       }
       error = ethers.utils.toUtf8String(errorBytes);
     }
@@ -116,7 +158,9 @@ const utilities = {
   },
 
   getEvent: async (txReceipt, emitter, eventName) => {
-    const receipt = await web3.eth.getTransactionReceipt(txReceipt.transactionHash);
+    const receipt = await web3.eth.getTransactionReceipt(
+      txReceipt.transactionHash
+    );
     const logs = await utilities.decodeLogs(receipt.logs, emitter, eventName);
     const event = logs.find((e) => e.event === eventName);
     return event;
@@ -135,38 +179,57 @@ const utilities = {
       address = null;
     }
 
-    const eventABIs = abi.filter((x) => x.type === "event" && x.name === eventName);
+    const eventABIs = abi.filter(
+      (x) => x.type === "event" && x.name === eventName
+    );
     if (eventABIs.length === 0) {
       throw new Error(`No ABI entry for event '${eventName}'`);
     } else if (eventABIs.length > 1) {
-      throw new Error(`Multiple ABI entries for event '${eventName}', only uniquely named events are supported`);
+      throw new Error(
+        `Multiple ABI entries for event '${eventName}', only uniquely named events are supported`
+      );
     }
 
     const [eventABI] = eventABIs;
 
     // The first topic will equal the hash of the event signature
-    const eventSignature = `${eventName}(${eventABI.inputs.map((input) => input.type).join(",")})`;
+    const eventSignature = `${eventName}(${eventABI.inputs
+      .map((input) => input.type)
+      .join(",")})`;
     const eventTopic = web3.utils.sha3(eventSignature);
 
     // Only decode events of type 'EventName'
     return logs
-      .filter((log) => log.topics.length > 0 && log.topics[0] === eventTopic && (!address || log.address === address))
-      .map((log) => web3.eth.abi.decodeLog(eventABI.inputs, log.data, log.topics.slice(1)))
+      .filter(
+        (log) =>
+          log.topics.length > 0 &&
+          log.topics[0] === eventTopic &&
+          (!address || log.address === address)
+      )
+      .map((log) =>
+        web3.eth.abi.decodeLog(eventABI.inputs, log.data, log.topics.slice(1))
+      )
       .map((decoded) => ({ event: eventName, args: decoded }));
   },
 
   versionFingerprint: (modules) => {
-    const concat = modules.map((module) => module.address).sort((m1, m2) => {
-      const bn1 = ethers.BigNumber.from(m1);
-      const bn2 = ethers.BigNumber.from(m2);
-      if (bn1.lt(bn2)) {
-        return 1;
-      }
-      if (bn1.gt(bn2)) {
-        return -1;
-      }
-      return 0;
-    }).reduce((prevValue, currentValue) => prevValue + currentValue.slice(2), "0x");
+    const concat = modules
+      .map((module) => module.address)
+      .sort((m1, m2) => {
+        const bn1 = ethers.BigNumber.from(m1);
+        const bn2 = ethers.BigNumber.from(m2);
+        if (bn1.lt(bn2)) {
+          return 1;
+        }
+        if (bn1.gt(bn2)) {
+          return -1;
+        }
+        return 0;
+      })
+      .reduce(
+        (prevValue, currentValue) => prevValue + currentValue.slice(2),
+        "0x"
+      );
     return ethers.utils.keccak256(concat).slice(0, 10);
   },
 
@@ -193,58 +256,85 @@ const utilities = {
   // return chainId;
   getChainId: async () => 1895,
 
-  web3GetClient: async () => new Promise((resolve, reject) => {
-    web3.eth.getNodeInfo((err, res) => {
-      if (err !== null) return reject(err);
-      return resolve(res);
-    });
-  }),
+  web3GetClient: async () =>
+    new Promise((resolve, reject) => {
+      web3.eth.getNodeInfo((err, res) => {
+        if (err !== null) return reject(err);
+        return resolve(res);
+      });
+    }),
 
   increaseTime: async (seconds) => {
     const client = await utilities.web3GetClient();
     if (!client.includes("TestRPC")) {
-      console.warning("Client is not ganache-cli and cannot forward time");
+      console.warn("Client is not ganache-cli and cannot forward time");
     } else {
       await utilities.evmIncreaseTime(seconds);
       await utilities.evmMine();
     }
   },
 
-  evmIncreaseTime: (seconds) => new Promise((resolve, reject) => web3.currentProvider.send(
-    {
-      jsonrpc: "2.0",
-      method: "evm_increaseTime",
-      params: [seconds],
-      id: 0,
-    },
-    (err, res) => (err ? reject(err) : resolve(res)))
-  ),
+  evmIncreaseTime: (seconds) =>
+    new Promise((resolve, reject) =>
+      web3.currentProvider.send(
+        {
+          jsonrpc: "2.0",
+          method: "evm_increaseTime",
+          params: [seconds],
+          id: 0,
+        },
+        (err, res) => (err ? reject(err) : resolve(res))
+      )
+    ),
 
-  evmMine: () => new Promise((resolve, reject) => web3.currentProvider.send(
-    {
-      jsonrpc: "2.0",
-      method: "evm_mine",
-      params: [],
-      id: 0,
-    },
-    (err, res) => (err ? reject(err) : resolve(res))
-  )),
+  evmMine: () =>
+    new Promise((resolve, reject) =>
+      web3.currentProvider.send(
+        {
+          jsonrpc: "2.0",
+          method: "evm_mine",
+          params: [],
+          id: 0,
+        },
+        (err, res) => (err ? reject(err) : resolve(res))
+      )
+    ),
 
   getNonceForRelay: async () => {
     const block = await web3.eth.getBlockNumber();
     const timestamp = new Date().getTime();
-    return `0x${ethers.utils.hexZeroPad(ethers.utils.hexlify(block), 16)
-      .slice(2)}${ethers.utils.hexZeroPad(ethers.utils.hexlify(timestamp), 16).slice(2)}`;
+    return `0x${ethers.utils
+      .hexZeroPad(ethers.utils.hexlify(block), 16)
+      .slice(2)}${ethers.utils
+      .hexZeroPad(ethers.utils.hexlify(timestamp), 16)
+      .slice(2)}`;
   },
 
   getNamedAccounts: async (accounts) => {
-    const addresses = accounts || await web3.eth.getAccounts();
-    const [infrastructure, owner, guardian1, relayer, tokenHolder, refundAddress, ...freeAccounts] = addresses;
-    return { infrastructure, owner, guardian1, relayer, tokenHolder, refundAddress, freeAccounts };
+    const addresses = accounts || (await web3.eth.getAccounts());
+    const [
+      infrastructure,
+      owner,
+      guardian1,
+      relayer,
+      tokenHolder,
+      refundAddress,
+      ...freeAccounts
+    ] = addresses;
+    return {
+      infrastructure,
+      owner,
+      guardian1,
+      relayer,
+      tokenHolder,
+      refundAddress,
+      freeAccounts,
+    };
   },
 
   encodeFunctionCall: (method, params) => {
-    if (typeof method === "object") return web3.eth.abi.encodeFunctionCall(method, params);
+    if (typeof method === "object")
+      return web3.eth.abi.encodeFunctionCall(method, params);
     if (typeof method === "string") {
       const paramStart = method.indexOf("(");
       const name = method.substring(0, paramStart);
@@ -252,7 +342,10 @@ const utilities = {
         .substring(paramStart + 1, method.length - 1)
         .split(",")
         .map((type, idx) => ({ type, name: `arg${idx}` }));
-      return web3.eth.abi.encodeFunctionCall({ name, inputs, type: "function" }, params);
+      return web3.eth.abi.encodeFunctionCall(
+        { name, inputs, type: "function" },
+        params
+      );
     }
     throw new Error(`Invalid method "${method}"`);
   },
@@ -263,14 +356,15 @@ const utilities = {
    * @param {Array<Transaction | [TruffleContract, string, any[], NumberLike?]} calls
    * @returns {Array<Transaction>}
    */
-  encodeCalls: (calls) => calls.map((call) => {
-    if (!Array.isArray(call)) {
-      return call;
-    }
-    const [instance, method, params = [], value = 0] = call;
-    const data = instance.contract.methods[method](...params).encodeABI();
-    return utilities.encodeTransaction(instance.address, value, data);
-  }),
+  encodeCalls: (calls) =>
+    calls.map((call) => {
+      if (!Array.isArray(call)) {
+        return call;
+      }
+      const [instance, method, params = [], value = 0] = call;
+      const data = instance.contract.methods[method](...params).encodeABI();
+      return utilities.encodeTransaction(instance.address, value, data);
+    }),
 
   addTrustedContact: async (wallet, target, module, securityPeriod) => {
     const owner = await wallet.owner();
@@ -282,16 +376,28 @@ const utilities = {
 
   // set the relayer nonce to > 0
   initNonce: async (wallet, module, manager, securityPeriod) => {
-    const nonceInitialiser = (await utilities.getNamedAccounts()).freeAccounts.slice(-1)[0];
-    await utilities.addTrustedContact(wallet, nonceInitialiser, module, securityPeriod);
+    const nonceInitialiser = (
+      await utilities.getNamedAccounts()
+    ).freeAccounts.slice(-1)[0];
+    await utilities.addTrustedContact(
+      wallet,
+      nonceInitialiser,
+      module,
+      securityPeriod
+    );
     const owner = await wallet.owner();
-    const transaction = utilities.encodeTransaction(nonceInitialiser, 1, ZERO_BYTES);
+    const transaction = utilities.encodeTransaction(
+      nonceInitialiser,
+      1,
+      ZERO_BYTES
+    );
     await manager.relay(
       module,
       "multiCall",
       [wallet.address, [transaction]],
       wallet,
-      [owner]);
+      [owner]
+    );
     const nonce = await module.getNonce(wallet.address);
     assert.isTrue(nonce.gt(0), "nonce init failed");
   },
@@ -302,7 +408,11 @@ const utilities = {
     assert.equal(error, msg);
   },
 
-  generateSaltValue: () => ethers.utils.hexZeroPad(ethers.BigNumber.from(ethers.utils.randomBytes(20)).toHexString(), 20),
+  generateSaltValue: () =>
+    ethers.utils.hexZeroPad(
+      ethers.BigNumber.from(ethers.utils.randomBytes(20)).toHexString(),
+      20
+    ),
 
   createWallet: async (factoryAddress, owner, modules, guardian) => {
     const salt = utilities.generateSaltValue();
@@ -310,15 +420,33 @@ const utilities = {
     const factory = await WalletFactory.at(factoryAddress);
 
     const tx = await factory.createCounterfactualWallet(
-      owner, modules, guardian, salt, 0, ethers.constants.AddressZero, ZERO_BYTES, managerSig);
+      owner,
+      modules,
+      guardian,
+      salt,
+      0,
+      ethers.constants.AddressZero,
+      ZERO_BYTES,
+      managerSig
+    );
 
-    const event = await utilities.getEvent(tx.receipt, factory, "WalletCreated");
+    const event = await utilities.getEvent(
+      tx.receipt,
+      factory,
+      "WalletCreated"
+    );
     return event.args.wallet;
   },
 
   swapAndCheckBalances: async ({ swap, bought, sold, wallet }) => {
-    const balanceOf = (token) => (token === ETH_TOKEN ? utilities.getBalance : token.balanceOf)(wallet.address);
-    const getBalances = async () => [await balanceOf(sold), await balanceOf(bought)];
+    const balanceOf = (token) =>
+      (token === ETH_TOKEN ? utilities.getBalance : token.balanceOf)(
+        wallet.address
+      );
+    const getBalances = async () => [
+      await balanceOf(sold),
+      await balanceOf(bought),
+    ];
 
     const [soldBefore, boughtBefore] = await getBalances();
     const result = await swap();
@@ -331,7 +459,7 @@ const utilities = {
     return result;
   },
 
-  usdcToWei: (amount) => new BN(amount).mul(new BN(1e6)).toString()
+  usdcToWei: (amount) => new BN(amount).mul(new BN(1e6)).toString(),
 };
 
 module.exports = utilities;
